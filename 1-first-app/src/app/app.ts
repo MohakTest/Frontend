eb4d0705740f9221744ae0c31bcace1a2517d6f9
MohakHarjani <mh2001harjani@gmail.com>
2025-12-06 11:59:03 +0530

    routing

--- 1-first-app/src/app/app.ts
+++ 1-first-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { BindingComponent } from './binding/binding.component';
 import { TwoWayBinding } from './two-way-binding/two-way-binding';
+import { Routing } from './routing/routing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TwoWayBinding],
+  imports: [Routing],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
